5a8e29dcc125fcc98c6005b06da182e5a93f33e1
Mathieu Benoit <mabenoit@microsoft.com>
2019-11-22 15:45:49 -0500

    convert the EchoBot to MyMonthlyBlogArticleBot

---
+++ MyMonthlyBlogArticle.Bot/Bots/MyMonthlyBlogArticleBot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace MyMonthlyBlogArticle.Bot.Bots
+{
+    public class MyMonthlyBlogArticleBot : ActivityHandler
+    {
+        private string WelcomeMessage = "Hello and welcome! I'm the 'Microsoft Azure News & Updates' Bot!";
+
+        private string HelpMessage
+        {
+            get
+            {
+                return $"You can search: \n* by month: *{DateTime.UtcNow.ToString("yyyy-MM")}* \n* by date: *{DateTime.UtcNow.ToString("yyyy-MM-dd")}* \n* by text: *Functions*, *\"API Management\"*, *DevOps*, *AKS | Kubernetes*, etc.";
+            }
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var text = turnContext.Activity.Text;
+            await turnContext.SendActivityAsync($"Echo: {text}", cancellationToken: cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(WelcomeMessage, cancellationToken: cancellationToken);
+                    await turnContext.SendActivityAsync(HelpMessage, cancellationToken: cancellationToken);
+                }
+            }
+        }
+    }
+}
--- MyMonthlyBlogArticle.Bot/Startup.cs
+++ MyMonthlyBlogArticle.Bot/Startup.cs
@@ -27,7 +27,7 @@ namespace MyMonthlyBlogArticle.Bot
             services.AddSingleton<IBotFrameworkHttpAdapter, MyMonthlyBlogArticleBotAdapter>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, EchoBot>();
+            services.AddTransient<IBot, MyMonthlyBlogArticleBot>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
