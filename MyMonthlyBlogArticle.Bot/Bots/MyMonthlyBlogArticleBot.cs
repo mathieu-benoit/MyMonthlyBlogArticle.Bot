52de0b199864d6f4bc08a0a2b52439e77d6be179
Mathieu Benoit <mabenoit@microsoft.com>
2019-11-28 23:08:01 -0500

    Azure Search implementation

--- MyMonthlyBlogArticle.Bot/Bots/MyMonthlyBlogArticleBot.cs
+++ MyMonthlyBlogArticle.Bot/Bots/MyMonthlyBlogArticleBot.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using MyMonthlyBlogArticle.Bot.Model;
+using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Schema;
 
 namespace MyMonthlyBlogArticle.Bot.Bots
 {
@@ -20,6 +25,13 @@ namespace MyMonthlyBlogArticle.Bot.Bots
             }
         }
 
+        private ISearchIndexClient SearchIndexClient { get; set; }
+
+        public MyMonthlyBlogArticleBot(IConfiguration configuration)
+        {
+            SearchIndexClient = new SearchIndexClient(configuration["AzureSearchServiceName"], configuration["AzureSearchIndexName"], new SearchCredentials(configuration["AzureSearchServiceQueryApiKey"]));
+        }
+
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             var text = turnContext.Activity.Text;
@@ -29,15 +41,15 @@ namespace MyMonthlyBlogArticle.Bot.Bots
             }
             else if (Regex.IsMatch(text, @"\d{4}\-\d{2}\-\d{2}"))
             {
-                await turnContext.SendActivityAsync($"Date: {text}", cancellationToken: cancellationToken);
+                await PostRssFeedsAsync(turnContext, cancellationToken, date: text);
             }
             else if (Regex.IsMatch(text, @"\d{4}\-\d{2}"))
             {
-                await turnContext.SendActivityAsync($"Month: {text}", cancellationToken: cancellationToken);
+                await PostRssFeedsAsync(turnContext, cancellationToken, month: text);
             }
             else
             {
-                await turnContext.SendActivityAsync($"Text: {text}", cancellationToken: cancellationToken);
+                await PostRssFeedsAsync(turnContext, cancellationToken, text: text);
             }
         }
 
@@ -51,6 +63,46 @@ namespace MyMonthlyBlogArticle.Bot.Bots
                     await turnContext.SendActivityAsync(HelpMessage, cancellationToken: cancellationToken);
                 }
             }
+        }
+
+        public async Task PostRssFeedsAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken, string date = null, string month = null, string text = null)
+        {
+            var results = GetRssFeeds(date, month, text);
+            var resultsCount = results.Count.Value;
+
+            if (resultsCount > 0)
+            {
+                var builder = new StringBuilder();
+                builder.Append($"{resultsCount} results found:");
+                foreach (var item in results.Results)
+                {
+                    // Need 2 spaces before each line for Teams channel.
+                    builder.Append($"  \n[{item.Document.Date}]({item.Document.Link}) - {item.Document.Title}");
+                }
+                await turnContext.SendActivityAsync($"{builder.ToString()}", cancellationToken: cancellationToken);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync($"No results found. \n{HelpMessage}", cancellationToken: cancellationToken);
+            }
+        }
+
+        private DocumentSearchResult<Feed> GetRssFeeds(string date = null, string month = null, string text = null)
+        {
+            var searchText = text;
+            var top = 1000;//that's the max allowed by the Azure Search API, otherwise by default it's 50. I don't think any search by bot at least will have more than 1000 documents returned for now... to keep in mind. Alternative: do paging.
+            var parameters = new SearchParameters() { OrderBy = new[] { "Date desc" }, Top = top, IncludeTotalResultCount = true };
+            if (!string.IsNullOrEmpty(month))
+            {
+                searchText = "*";
+                parameters.Filter = $"PartitionKey eq '{month}'";
+            }
+            else if (!string.IsNullOrEmpty(date))
+            {
+                searchText = "*";
+                parameters.Filter = $"Date eq '{date}'";
+            }
+            return SearchIndexClient.Documents.Search<Feed>(searchText, parameters);
         }
     }
 }
