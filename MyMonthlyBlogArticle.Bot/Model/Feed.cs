8acc6d54db26db7b886ff3357413042a911df70b
Mathieu Benoit <mabenoit@microsoft.com>
2019-11-28 23:08:34 -0500

    Add Feed.cs

---
+++ MyMonthlyBlogArticle.Bot/Model/Feed.cs
@@ -0,0 +1,16 @@
+﻿namespace MyMonthlyBlogArticle.Bot.Model
+{
+    public class Feed
+    {
+        //Key --> Key
+        //PartitionKey --> Filterable, Facetable?
+
+        public string Date { get; set; }//Retrievable, Filterable, Sortable
+
+        public string Title { get; set; }//Retrievable, Searchable
+
+        public string Link { get; set; }//Retrievable, Searchable
+
+        public string Type { get; set; }//Facetable?
+    }
+}
