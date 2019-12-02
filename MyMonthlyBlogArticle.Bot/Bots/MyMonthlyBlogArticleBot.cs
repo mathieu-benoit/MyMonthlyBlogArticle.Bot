dbd6bc84f350a1aecea72c0e76b6bfbf28221223
Mathieu Benoit <mabenoit@microsoft.com>
2019-12-02 08:55:15 -0500

    search ENVs + trackevents in appinsights

--- MyMonthlyBlogArticle.Bot/Bots/MyMonthlyBlogArticleBot.cs
+++ MyMonthlyBlogArticle.Bot/Bots/MyMonthlyBlogArticleBot.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Search;
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.Azure.Search;
 using Microsoft.Azure.Search.Models;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
@@ -15,6 +16,8 @@ namespace MyMonthlyBlogArticle.Bot.Bots
 {
     public class MyMonthlyBlogArticleBot : ActivityHandler
     {
+        private TelemetryClient Telemetry { get; set; }
+
         private string WelcomeMessage = "Hello and welcome! I'm the 'Microsoft Azure News & Updates' Bot!";
 
         private string HelpMessage
@@ -25,11 +28,16 @@ namespace MyMonthlyBlogArticle.Bot.Bots
             }
         }
 
+        private string AzureSearchServiceName { get; set; }
+        private string AzureSearchIndexName { get; set; }
         private ISearchIndexClient SearchIndexClient { get; set; }
 
-        public MyMonthlyBlogArticleBot(IConfiguration configuration)
+        public MyMonthlyBlogArticleBot(IConfiguration configuration, TelemetryClient telemetryClient)
         {
-            SearchIndexClient = new SearchIndexClient(configuration["AzureSearchServiceName"], configuration["AzureSearchIndexName"], new SearchCredentials(configuration["AzureSearchServiceQueryApiKey"]));
+            AzureSearchServiceName = configuration["AZURE_SEARCH_SERVICE_NAME"];
+            AzureSearchIndexName = configuration["AZURE_SEARCH_INDEX_NAME"];
+            SearchIndexClient = new SearchIndexClient(AzureSearchServiceName, AzureSearchIndexName, new SearchCredentials(configuration["AZURE_SEARCH_SERVICE_QUERY_API_KEY"]));
+            Telemetry = telemetryClient;
         }
 
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
@@ -37,6 +45,8 @@ namespace MyMonthlyBlogArticle.Bot.Bots
             var text = turnContext.Activity.Text;
             if (Regex.IsMatch(text, "^Hello|hello|Hi|hi|Hey|hey|Help|help|What|what"))
             {
+                var properties = new Dictionary<string, string> { { "Text", text } };
+                Telemetry.TrackEvent("Hello", properties);
                 await turnContext.SendActivityAsync(HelpMessage, cancellationToken: cancellationToken);
             }
             else if (Regex.IsMatch(text, @"\d{4}\-\d{2}\-\d{2}"))
@@ -67,8 +77,12 @@ namespace MyMonthlyBlogArticle.Bot.Bots
 
         public async Task PostRssFeedsAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken, string date = null, string month = null, string text = null)
         {
+            var startTime = DateTime.UtcNow;
+            var timer = System.Diagnostics.Stopwatch.StartNew();
             var results = GetRssFeeds(date, month, text);
             var resultsCount = results.Count.Value;
+            var timerElapsed = timer.ElapsedMilliseconds;
+            TrackEvent(date, month, text, resultsCount, timerElapsed);
 
             if (resultsCount > 0)
             {
@@ -103,6 +117,19 @@ namespace MyMonthlyBlogArticle.Bot.Bots
                 parameters.Filter = $"Date eq '{date}'";
             }
             return SearchIndexClient.Documents.Search<Feed>(searchText, parameters);
+        }
+
+        private void TrackEvent(string date, string month, string text, long resultsCount, long elapsedTime)
+        {
+            var properties = new Dictionary<string, string> {
+                {"SearchServiceName", AzureSearchServiceName},
+                {"IndexName", AzureSearchIndexName},
+                {"QueryTerms", date ?? month ?? text},
+                {"ResultCount", resultsCount.ToString()},
+                {"SearchType", !string.IsNullOrEmpty(date) ? "Date" : !string.IsNullOrEmpty(month) ? "Month" : "Text"},
+                {"SearchTimeElapsed", elapsedTime.ToString()}
+            };
+            Telemetry.TrackEvent("Search", properties);
         }
     }
 }
