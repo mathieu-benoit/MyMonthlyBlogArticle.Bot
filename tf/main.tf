7e052c5e72ba71ff66b09d9efc3b9de8877a16e7
Mathieu Benoit <mabenoit@microsoft.com>
2020-01-01 18:54:59 -0500

    azurerm_management_lock
    Signed-off-by: Mathieu Benoit <mabenoit@microsoft.com>

--- tf/main.tf
+++ tf/main.tf
@@ -7,6 +7,13 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_management_lock" "lock" {
+  name       = "CanNotDelete"
+  scope      = azurerm_resource_group.rg.id
+  lock_level = "CanNotDelete"
+  notes      = "This Resource Group and its Resources can't be deleted."
+}
+
 resource "azurerm_application_insights" "ai" {
   name                = var.bot_name
   location            = var.location
