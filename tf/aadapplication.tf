29dd47ade96824df3124d10c0b7b5dff97a07a52
Mathieu Benoit <mabenoit@microsoft.com>
2020-02-23 09:25:51 -0500

    [skip ci] tmp disable aad app

--- tf/aadapplication.tf
+++ tf/aadapplication.tf
@@ -1,21 +1,21 @@
-resource "random_password" "password" {
-  length  = 16
-  special = true
-  min_numeric = 1
-  min_special = 1
-}
+#resource "random_password" "password" {
+#  length  = 16
+#  special = true
+#  min_numeric = 1
+#  min_special = 1
+#}
 
-resource "azuread_application" "app" {
-  name                       = var.bot_name
-  available_to_other_tenants = false
-}
+#resource "azuread_application" "app" {
+#  name                       = var.bot_name
+#  available_to_other_tenants = false
+#}
 
-resource "azuread_application_password" "password" {
-  application_object_id = azuread_application.app.object_id
-  value                 = random_password.password.result
-  end_date              = var.azuread_application_password_end_date
+#resource "azuread_application_password" "password" {
+#  application_object_id = azuread_application.app.object_id
+#  value                 = random_password.password.result
+#  end_date              = var.azuread_application_password_end_date
 
-  lifecycle {
-    ignore_changes = [end_date]
-  }
-}
+#  lifecycle {
+#    ignore_changes = [end_date]
+#  }
+#}
